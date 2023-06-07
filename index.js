8596fd714de5e15ebc81352030b8495f9abdad25
Dmitri Sanzharov <dmitrisanzharov@gmail.com>
2023-06-07 03:17:09 +0100

    dsf

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ export const carsJSON = [
 		id: "646f18c67a2f3482720d70cf",
 		name: "red-car-1",
 		color: "lightcoral",
-		icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/small-car-2392474-1993928.png?f=webp&w=48",
-		location: [53.38970888408873, -6.22450543869382],
 		rentedOut: "not-available",
 		miles: 50000,
 	},
@@ -12,8 +10,6 @@ export const carsJSON = [
 		id: "646f18c67a2f3482720d70d0",
 		name: "red-car-2",
 		color: "lightcoral",
-		location: [53.37144791348412, -6.269594706494894],
-		icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/small-car-2392474-1993928.png?f=webp&w=48",
 		rentedOut: "available",
 		miles: 75000,
 	},
@@ -21,8 +17,6 @@ export const carsJSON = [
 		id: "646f18c67a2f3482720d70d8",
 		name: "red-car-3",
 		color: "lightcoral",
-		location: [53.42689512951666, -6.230291464681101],
-		icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/small-car-2392474-1993928.png?f=webp&w=48",
 		rentedOut: "not-available",
 		miles: 275000,
 	},
@@ -30,8 +24,6 @@ export const carsJSON = [
 		id: "646f18c67a2f3482720d70d1",
 		name: "blue-car-1",
 		color: "lightcyan",
-		location: [53.357605, -6.223773],
-		icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/cute-car-2392497-1993951.png?f=webp&w=48",
 		rentedOut: "not-available",
 		miles: 100000,
 	},
@@ -39,8 +31,6 @@ export const carsJSON = [
 		id: "646f18c67a2f3482720d70d2",
 		name: "blue-car-2",
 		color: "lightcyan",
-		location: [53.38738840701002, -6.1814709180642105],
-		icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/cute-car-2392497-1993951.png?f=webp&w=48",
 		rentedOut: "available",
 		miles: 125000,
 	},
@@ -48,8 +38,6 @@ export const carsJSON = [
 		id: "646f18c67a2f3482720d70d3",
 		name: "orange-car-1",
 		color: "orange",
-		location: [53.39284492102931, -6.249195583637723],
-		icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/laughing-car-2392488-1993942.png?f=webp&w=48",
 		rentedOut: "not-available",
 		miles: 150000,
 	},
@@ -57,8 +45,6 @@ export const carsJSON = [
 		id: "646f18c67a2f3482720d70d4",
 		name: "pink-car-1",
 		color: "pink",
-		location: [53.406655991806815, -6.235034806528632],
-		icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/vintage-car-2392492-1993946.png?f=webp&w=48",
 		rentedOut: "not-available",
 		miles: 175000,
 	},
@@ -66,8 +52,6 @@ export const carsJSON = [
 		id: "646f18c67a2f3482720d70d5",
 		name: "beige-car-1",
 		color: "beige",
-		location: [53.38604265279719, -6.256020406774416],
-		icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/jeep-2392484-1993938.png?f=webp&w=48",
 		rentedOut: "available",
 		miles: 200000,
 	},
@@ -75,8 +59,6 @@ export const carsJSON = [
 		id: "646f18c67a2f3482720d70d6",
 		name: "yellow-car-1",
 		color: "yellow",
-		location: [53.37603837534987, -6.239767880604335],
-		icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/open-roof-car-2392496-1993950.png?f=webp&w=48",
 		rentedOut: "not-available",
 		miles: 225000,
 	},
@@ -84,8 +66,6 @@ export const carsJSON = [
 		id: "646f18c67a2f3482720d70d7",
 		name: "yellow-car-2",
 		color: "yellow",
-		location: [53.3766346285211, -6.169640948151451],
-		icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/open-roof-car-2392496-1993950.png?f=webp&w=48",
 		rentedOut: "available",
 		miles: 250000,
 	},
@@ -99,48 +79,30 @@ export const carsJSON = [
 // ];
 
 const filters1 = {
-	color: "lightcoral",
-	rentedOut: "not-available",
+	color: ["yellow", "lightcoral"],
+	rentedOut: ["not-available"],
 };
 
-function filterArray(arr, filt) {
-	//? STAGE 1: first I need to make sure that every item has KEYS in it
-	let filterKeysArr = Object.keys(filt);
-	console.log("filterKeysArr: ", filterKeysArr);
+function filterData(arr, filt) {
+	const filterKeysArray = Object.keys(filt);
+	// console.log("filterKeysArray: ", filterKeysArray);
 
-	let hasFilterKeysArr = arr.filter((item) => {
-		return filterKeysArr.every((filterKey) => {
-			return item[filterKey];
+	return arr.filter((item) => {
+		return filterKeysArray.every((key) => {
+			let itemValue = item[key];
+			// console.log("itemValue: ", itemValue);
+			let filterValue = filt[key]; // so here I need to insert INFINITE OR statements
+			// console.log("filterValue: ", filterValue);
+			// console.log("==========");
+			return filterValue.indexOf(itemValue) !== -1;
 		});
 	});
-	// console.log(hasFilterKeysArr.length);
-	// console.log(hasFilterKeysArr);
 
-	//? Stage 2: now I need to make sure that every KEY in item is one of the items from the filters
+	// remove duplicates
 
-	let final1 = [];
-
-	hasFilterKeysArr.forEach((item) => {
-		// need to get all keys and all filter values
-
-		let test1 = filterKeysArr.every((filterKey) => {
-			// console.log(filterKey);
-			// console.log(item[filterKey]);
-			// console.log(filters1[filterKey]);
-			return item[filterKey] === filters1[filterKey];
-		});
-		console.log("=================");
-		console.log(test1);
-
-		if (test1) {
-			final1.push(item);
-		}
-	});
-
-	console.log(final1.length);
-	console.log(final1);
-
-	// end of the function
+	// let final2 = Array.from(new Set(final1));
+	// console.log("final2: ", final2);
 }
 
-filterArray(carsJSON, filters1);
+let a = filterData(carsJSON, filters1);
+console.log(a);
